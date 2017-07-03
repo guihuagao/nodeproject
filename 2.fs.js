cd007f3fd91697c6ac4593b33a250bfc086f55fd
zhanglm <2475352924@qq.com>
2017-07-03 09:57:32 +0800

    更新代码

--- 2.fs.js
+++ 2.fs.js
@@ -53,10 +53,30 @@ var data='这是一个即将被写入文件的文字';
 fs.writeFile()
     将数据写入到文件中，如果文件不存在自动创建
     写入时首先将原有数据清空，再次写入
- */
 
-fs.writeFile('./write.txt',data,function (err) {
+
+  在./write.txt文件中写入内容
+
+  第一个参数  指定要写入数据的文件
+  第二个参数  data只要写入的数据
+  第三个参数  指定写入的模式、编码字符集
+             {
+             //写入模式为追加写
+             flag:'a'
+             }
+  第四个参数  是callback回调函数
+
+{flag:'a'}追加写
+
+ */
+fs.writeFile('./write.txt',{flag:'a'},data,function (err) {
  console.log(err);
 
+});
+
+//另一种追加写的方法 fs.appendFile()
+
+fs.appendFile('./write.txt',data,function (err) {
+    console.log(err);
 })
 
